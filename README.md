f1eb01c2864f3d1f0abfd7c0457b9dbb131f8ce2
Rick <rickb777@github.com>
2015-06-11 22:00:44 +0100

    Manually merged forward all the FCGI code, along with the pluggable logger and the nice API for signal handling.

--- README.md
+++ README.md
@@ -23,6 +23,34 @@ Manners ensures that all requests are served by incrementing a WaitGroup when a 
 
 If your request handler spawns Goroutines that are not guaranteed to finish with the request, you can ensure they are also completed with the `StartRoutine` and `FinishRoutine` functions on the server.
 
+### HTTP, HTTPS and FCGI
+
+Manners supports three protocols: HTTP, HTTPS and FCGI. HTTP is illustrated above. 
+For HTTPS, Manners can likewise act as a drop-in replacement for the standard library's 
+[http.ListenAndServeTLS](http://golang.org/pkg/net/http/#ListenAndServeTLS) function:
+
+```go
+func main() {
+  handler  := MyHTTPHandler()
+  certFile := MyCertificate()
+  keyFile  := MyKeyFile()
+  manners.ListenAndServeTLS(":https", certFile, keyFile, handler)
+}
+```
+
+In Manners, FCGI only operates via local a Unix socket connected to a co-hosted proxy, such as Apache or Nginx. 
+
+```go
+func main() {
+  handler := MyHTTPHandler()
+  manners.ListenAndServe("/var/run/goserver.sock", handler)
+}
+```
+
+To use FCGI, the port string must specify the Unix socket and start with a slash or dot, as in the example above. In this case, Manners will use [fcgi.Serve](http://golang.org/pkg/net/http/fcgi/#Serve).
+
+In each of the protocols, Manners drains down the connections cleanly when `manners.Close()` is called.
+
 ### Compatability
 
 Manners 0.3.0 and above uses standard library functionality introduced in Go 1.3.
--- server.go
+++ server.go
@@ -27,14 +27,7 @@ or for a customized server:
 
 The server will shut down cleanly when the Close() method is called:
 
-	go func() {
-		sigchan := make(chan os.Signal, 1)
-		signal.Notify(sigchan, os.Interrupt, os.Kill)
-		<-sigchan
-		log.Info("Shutting down...")
-		manners.Close()
-	}()
-
+	manners.CloseOnInterrupt()
 	http.Handle("/hello", myHandler)
 	log.Fatal(manners.ListenAndServe(":8080", nil))
 */
@@ -46,6 +39,12 @@ import (
 	"net/http"
 	"sync"
 	"sync/atomic"
+	"strings"
+	"os"
+	"fmt"
+	"net/http/fcgi"
+	"os/signal"
+	"syscall"
 )
 
 // A GracefulServer maintains a WaitGroup that counts how many in-flight
@@ -83,16 +82,52 @@ func NewWithServer(s *http.Server) *GracefulServer {
 // Close stops the server from accepting new requets and begins shutting down.
 // It returns true if it's the first time Close is called.
 func (s *GracefulServer) Close() bool {
+	logger.Printf("Shutting down server on %s\n", s.Server.Addr)
 	return <-s.shutdown
 }
 
-// ListenAndServe provides a graceful equivalent of net/http.Serve.ListenAndServe.
+func isUnixNetwork(addr string) bool {
+	return strings.HasPrefix(addr, "/") || strings.HasPrefix(addr, ".")
+}
+
+func listenToUnix(bind string) (listener net.Listener, err error) {
+	_, err = os.Stat(bind)
+	if err == nil {
+		// socket exists and is "already in use";
+		// presume this is from earlier run and therefore delete it
+		err = os.Remove(bind)
+		if err != nil {
+			return
+		}
+	} else if !os.IsNotExist(err) {
+		return
+	}
+	listener, err = net.Listen("unix", bind)
+	return
+}
+
+func listen(bind string) (listener net.Listener, err error) {
+	if isUnixNetwork(bind) {
+		logger.Printf("Listening on unix socket %s\n", bind)
+		return listenToUnix(bind)
+	} else if strings.Contains(bind, ":") {
+		logger.Printf("Listening on tcp socket %s\n", bind)
+		return net.Listen("tcp", bind)
+	} else {
+		return nil, fmt.Errorf("error while parsing bind arg %v", bind)
+	}
+}
+
+// ListenAndServe provides a graceful equivalent of net/http.Server.ListenAndServe.
+// This supports HTTP and FCGI but not HTTPS. For HTTP, the `addr` will contain a colon,
+// e.g. ":8001". To use FCGI, a Unix socket name must be supplied for `addr` which
+// must begin with '/' or '.'.
 func (s *GracefulServer) ListenAndServe() error {
 	addr := s.Addr
 	if addr == "" {
 		addr = ":http"
 	}
-	listener, err := net.Listen("tcp", addr)
+	listener, err := listen(addr)
 	if err != nil {
 		return err
 	}
@@ -100,7 +135,8 @@ func (s *GracefulServer) ListenAndServe() error {
 	return s.Serve(listener)
 }
 
-// ListenAndServeTLS provides a graceful equivalent of net/http.Serve.ListenAndServeTLS.
+// ListenAndServeTLS provides a graceful equivalent of net/http.Server.ListenAndServeTLS.
+// This supports HTTPS only (not HTTP or FCGI).
 func (s *GracefulServer) ListenAndServeTLS(certFile, keyFile string) error {
 	// direct lift from net/http/server.go
 	addr := s.Addr
@@ -202,7 +238,13 @@ func (s *GracefulServer) Serve(listener net.Listener) error {
 		s.up <- listener
 	}
 
-	err := s.Server.Serve(listener)
+	var err error
+	if isUnixNetwork(s.Server.Addr) {
+		os.Chmod(s.Server.Addr, os.ModePerm)
+		err = fcgi.Serve(listener, s.Server.Handler)
+	} else {
+		err = s.Server.Serve(listener)
+	}
 
 	// This block is reached when the server has received a shut down command
 	// or a real error happened.
@@ -225,4 +267,22 @@ func (s *GracefulServer) StartRoutine() {
 // StartRoutine().
 func (s *GracefulServer) FinishRoutine() {
 	s.wg.Done()
+}
+
+// CloseOnInterrupt creates a go-routine that will call the Close() function when certain OS
+// signals are received. If no signals are specified,
+// the following are used: SIGINT, SIGTERM, SIGKILL, SIGQUIT, SIGHUP, SIGUSR1.
+// This function must be called before ListenAndServe.
+func CloseOnInterrupt(signals ...os.Signal) {
+	go func() {
+		sigchan := make(chan os.Signal, 1)
+		if len(signals) > 0 {
+			signal.Notify(sigchan, signals...)
+		} else {
+			signal.Notify(sigchan, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL,
+				syscall.SIGQUIT, syscall.SIGHUP, syscall.SIGUSR1)
+		}
+		<-sigchan
+		Close()
+	}()
 }
